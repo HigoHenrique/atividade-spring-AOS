3acc96ac23d958ae16f05fc64ee4d0b353d6afc0
HigoHenrique <99215544+HigoHenrique@users.noreply.github.com>
2023-05-07 17:53:54 -0300

    criando projeto spring para atividade de Aplicações Orientada a Serviço

---
+++ src/main/java/com/example/atvspringboot/AtvSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.atvspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtvSpringBootApplication.class, args);
+    }
+
+}
